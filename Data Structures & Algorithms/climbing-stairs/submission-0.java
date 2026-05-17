90d7f286bb46a1345c9c3efa2b455bc00cab6a80
zzonni <89135352+zzonni@users.noreply.github.com>
2026-05-17 17:16:47 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int prev2 = 1, prev1 = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            int current = prev1+prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
